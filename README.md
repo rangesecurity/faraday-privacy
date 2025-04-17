e109a3b16045eb153f144b48a75b31f134c2c278
bonedaddy <alex@bonedaddy.io>
2025-04-16 19:44:15 -0700

    wip disclosure client

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # disclosure-library
+
+# Development
+
+## Penumbra
+
+Clone the penumbra repository to setup the development environment
+
+```shell
+$> git clone https://github.com/penumbra-zone/penumbra
+$> cd penumbra
+$> git checkout v1.4.0
+```
+
+Setup the development environment
+
+```shell
+$> nix develop
+$> just dev
+```
+
+
+# Resources
+
+## Penumbra
+
+* https://github.com/penumbra-zone/penumbra/blob/main/crates/bin/pcli/src/opt.rs#L65
--- crates/penumbra/src/lib.rs
+++ crates/penumbra/src/lib.rs
@@ -1,14 +1,44 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use {
+    anyhow::{Context, Result},
+    penumbra_sdk_proto::{
+        box_grpc_svc::{self, BoxGrpcService},
+        view::v1::view_service_client::ViewServiceClient,
+    },
+    penumbra_sdk_view::ViewClient,
+    tonic::transport::Channel,
+};
+
+pub struct DisclosureClient {
+    view: ViewServiceClient<Channel>,
+}
+
+impl DisclosureClient {
+    pub async fn new(url: &str) -> Result<Self> {
+        let view_service = ViewServiceClient::connect(url.to_string())
+            .await
+            .with_context(|| "failed to connect ViewServiceClient")?;
+        Ok(Self { view: view_service })
+    }
+}
+
+impl AsMut<ViewServiceClient<Channel>> for DisclosureClient {
+    fn as_mut(&mut self) -> &mut ViewServiceClient<Channel> {
+        &mut self.view
+    }
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
+mod test {
+    use penumbra_sdk_proto::view::v1::StatusRequest;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    use super::*;
+    #[tokio::test]
+    async fn test_disclosure_client_new() {
+        let mut dc = DisclosureClient::new("http://localhost:8080")
+            .await
+            .unwrap();
+        let view_service: &mut ViewServiceClient<Channel> = dc.as_mut();
+        let res = view_service.status(StatusRequest {}).await.unwrap();
+        println!("{res:#?}");
     }
 }
