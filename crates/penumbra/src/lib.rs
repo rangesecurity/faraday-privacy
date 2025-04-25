9af15738e06694707985499af312dcd6fc6779fb
bonedaddy <alex@bonedaddy.io>
2025-04-24 17:13:45 -0700

    get timestamp for block tx is included in

--- crates/penumbra/src/lib.rs
+++ crates/penumbra/src/lib.rs
@@ -1,9 +1,17 @@
 use {
     anyhow::{Context, Result},
     camino::Utf8PathBuf,
+    common::{
+        self,
+        models::{counterparty::Role, transaction::Protocol, Counterparty, Transaction},
+    },
+    futures::StreamExt,
     penumbra_sdk_keys::FullViewingKey,
     penumbra_sdk_proto::{
         box_grpc_svc::{self, BoxGrpcService},
+        util::tendermint_proxy::v1::{
+            tendermint_proxy_service_client::TendermintProxyServiceClient, GetBlockByHeightRequest,
+        },
         view::v1::{
             view_service_client::ViewServiceClient, view_service_server::ViewServiceServer,
         },
@@ -11,7 +19,6 @@ use {
     penumbra_sdk_view::{ViewClient, ViewServer},
     std::str::FromStr,
     tonic::transport::Channel,
-    futures::StreamExt
 };
 
 pub struct DisclosureClient {
@@ -37,11 +44,54 @@ impl DisclosureClient {
     }
     pub async fn sync(&mut self) -> Result<()> {
         let mut stream = self.view().status_stream().await?;
-        while let Some(Ok(res)) = stream.next().await {}
+        while let Some(Ok(_)) = stream.next().await {}
         Ok(())
     }
     pub fn view(&mut self) -> &mut impl ViewClient {
         &mut self.view
+    }
+    pub async fn transaction(&mut self, hash: &str) -> Result<Transaction> {
+        let txn = self
+            .view()
+            .transaction_info_by_hash(hash.parse().with_context(|| "failed to parse hash")?)
+            .await?;
+
+        let mut tpc = TendermintProxyServiceClient::connect("http://localhost:8080").await?;
+
+        let time = tpc
+            .get_block_by_height(GetBlockByHeightRequest {
+                height: txn.height as i64,
+            })
+            .await
+            .with_context(|| "failed to query blockheight")?
+            .into_inner()
+            .block
+            .with_context(|| "block is None")?
+            .header
+            .with_context(|| "header is None")?
+            .time
+            .with_context(|| "time is None")?;
+
+        let mut tx = Transaction {
+            transaction_hash: hash.to_string(),
+            protocol: Protocol::Penumbra,
+            chain_id: txn.id.to_string(),
+            counterparties: vec![],
+            // todo: convert timestamp
+            timestamp: format!("{}", time.seconds),
+            metadata: None,
+        };
+
+        for effect in txn.summary.effects {
+            tx.counterparties.push(Counterparty {
+                // this is not correct, need a better wat to determine if it is the recipient or receiver
+                role: Role::Receiver,
+                address: effect.address.address().to_string(),
+                name: None,
+                assets: vec![],
+            })
+        }
+        Ok(tx)
     }
 }
 
@@ -81,17 +131,24 @@ mod test {
         let vc = dc.view();
 
         let res = vc.balances(AddressIndex::new(0), None).await.unwrap();
-        println!("{res:#?}");
 
         let res = vc
             .transaction_info_by_hash(
-                "74f2f7e67137ccbbe14636180acc22e1d1ce2987dcc0ec6f9c1e8a88b0738b88"
-                  //  .to_uppercase()
+                "c888fe430188c9a83aa450ab7f647c51f6224caf16e3b8b25177d5d9d300ccaf"
+                    //  .to_uppercase()
                     .parse()
                     .unwrap(),
             )
             .await
             .unwrap();
-        println!("{res:#?}");
+        println!("{:#?}", res.summary);
+        // print memo view (contains address information)
+        // println!("{:#?}", res.view.body_view.memo_view);
+
+        // print transaction parameters (contains chain information)
+        // println!("{:#?}", res.view.body_view.transaction_parameters);
+
+        // print action views (contains tx summaries, etc...)
+        // println!("{:#?}", res.view.body_view.action_views);
     }
 }
