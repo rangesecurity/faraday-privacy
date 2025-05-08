a03c84dee2355a2d33ef9cfaca13c3fbe2e3a0a7
bonedaddy <alex@bonedaddy.io>
2025-05-08 02:46:06 -0700

    add lint script

---
+++ scripts/lint.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+CLIPPY_FLAGS=(
+    "-D" "warnings"                # Make all warnings deny
+    "-W" "clippy::all"            # All default lints
+    "-W" "clippy::cargo"          # Cargo-related lints
+    "-D" "clippy::perf"           # Performance-related lints
+    "-D" "clippy::complexity"     # Code complexity lints
+    "-D" "clippy::style"          # Style lints
+    # Exclude some overly strict lints
+    "-A" "clippy::must_use_candidate"
+    "-A" "clippy::missing_errors_doc"
+    "-A" "clippy::module_name_repetitions"
+    "-A" "clippy::multiple_crate_versions"
+    "-A" "clippy::cargo_common_metadata"
+    "-A" "clippy::negative_feature_names"
+)
+
+# Run clippy on all targets (including tests and examples)
+cargo clippy --fix -- "${CLIPPY_FLAGS[@]}" "$1"
+
